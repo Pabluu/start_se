c5bc684b69989485d64b93f3b21e8ede2505614e
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2022-06-18 21:15:56 -0300

    operadores parte 1

---
+++ Modulo 07 - JavaScript/06-operadores-i.js
@@ -0,0 +1,87 @@
+// OPERADORES ARITMÉTICOS
+
+
+// concatenação + 
+let bemVindo='Olá ', nome = 'Carlos';
+let msg = bemVindo + nome;
+
+console.log(msg);
+
+// soma +
+let a = 6, b=9;
+let soma = a+b;
+
+console.log(soma);
+
+//multiplicação *
+mult = 8 * 11;
+console.log(mult);
+
+// divisão /
+divi = 12/4;
+console.log(divi);
+
+// resto %
+resto = 12 % 3;
+console.log(resto);
+
+
+
+
+//OPERADORES DE ATRIBUIÇÃO
+
+// igual =
+let cor = '#eb5600';
+console.log(cor);
+
+// multiplicar a si mesmo *=
+let num = 3;
+console.log(num *= 2);
+
+// elevar a si mesmo *=
+let num1 = 3;
+console.log(num1 **= 2);
+
+// dividir  a si mesmo *=
+let num2 = 3;
+console.log(num2 /= 2);
+
+// dividir  a si mesmo *=
+let num3 = 3;
+console.log(num3 /= 2);
+
+// somar  a si mesmo *=
+let num4 = 3;
+console.log(num4 += 2);
+
+// subtrair  a si mesmo *=
+let num5 = 3;
+console.log(num5 -= 2);
+
+
+// OPERADORES RELACIONAIS
+var x = 10, y = 20;
+
+// maior > 
+console.log(a > 10); //false
+
+// menor < 
+console.log(b < 21); //true
+
+// maior igual >= 
+console.log(a >= 10); //true
+
+// menor igual <=
+console.log(b <= 21); //true
+
+
+
+// OPERADORES DE IGUALDADE
+var nota1 = 8;
+var nota2 = '8';
+
+// Não considera o tipo de dado == 
+console.log(nota1 == nota2);
+
+// Considera o tipo de dado ===
+console.log(nota1 === nota2);
