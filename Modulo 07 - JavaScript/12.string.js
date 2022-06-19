e3f1b46408c1fdfb6ee0f4afd979932ac648e189
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2022-06-19 12:05:56 -0300

    trabalhando com string - cadeia de caracteres

---
+++ Modulo 07 - JavaScript/12.string.js
@@ -0,0 +1,21 @@
+// STRING - CADEIA DE CARACTERES
+
+let nome = 'Ana';
+
+let idade = 20;
+
+let conc = `${nome}, ${idade} anos`;
+console.log(conc);
+
+console.log(nome.lenght);
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
+
+let localizacao = 'Curitiba - PR';
+// pegando uma parte da string
+let estado = localizacao.substring(localizacao.lenght - 2);
+console.log(estado);
+
+let texto = 'basquete';
+// substituir uma (sub)string por outra
+let novoTexto = texto.replace('bas', 'ra');
