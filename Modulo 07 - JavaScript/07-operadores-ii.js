bdea4d50e6d4535d2c7b6a16618c8502e1f31500
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2022-06-18 21:16:15 -0300

    operadores parte 2

---
+++ Modulo 07 - JavaScript/07-operadores-ii.js
@@ -0,0 +1,48 @@
+// OPERADOR NEGAÇÃO - !
+
+// !
+let conectado =  true;
+
+// true
+console.log(conectado);
+
+// negando
+console.log(!conectado); //false
+
+
+// DESIGUALDADE
+let nota1 = 5;
+let nota2 = '5';
+
+console.log(nota1 != nota2); //false
+console.log(nota1 !== nota2); //true
+
+
+
+// OPERADORES LÓGICOS BINÁRIOS
+
+var a = 2;
+let b = 4;
+let c = 6;
+let d = 8;
+
+// AND &&
+console.log((a > b) && (c > b));
+
+
+// OR ||
+console.log((d > b) || (c > b));
+
+
+// OPERADOR INCREMENTAL
+
+// MAIS 1 ++
+var x = 10;
+var a = ++x;
+
+
+// OPERADOR DECREMENTAL
+
+// MENOS 1 --
+var x = 10;
+var a = --x;
