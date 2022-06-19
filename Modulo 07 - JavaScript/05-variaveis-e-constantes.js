7a5f9f22da001b3932de0da44e4bbac2b1ca0d10
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2022-06-18 21:14:52 -0300

    trabalhando com variaveis e constantes

---
+++ Modulo 07 - JavaScript/05-variaveis-e-constantes.js
@@ -0,0 +1,24 @@
+//vars diferentes por causa do case sensitive
+let primeiroNome = 'Pablos';
+let PrimeiroNome = 'Anas';
+
+let sobreNome = 'Henrique';
+// sobrescrevendo a var
+sobreNome = 'Silva';
+
+let a  = 2;
+let b = 3;
+
+// ou
+// let a = 2, b = 3;
+
+let soma = a + b;
+console.log(soma);
+
+// constante
+const pi = 3.1416
+let raio = 3;
+let circuferencia = 2 * pi * raio;
+console.log(circuferencia);
+pi = 3;
+console.log(pi);
