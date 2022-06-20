9005cf38080ff00943ba27566bfe42eb5acc1d41
Pablo Henrique da Silva Cunha <pabloph0077@gmail.com>
2022-06-20 10:16:27 -0300

    trabalhando com arrays parte 1

---
+++ Modulo 07 - JavaScript/13-array-i.js
@@ -0,0 +1,23 @@
+// ARRAY - ARRANJO - VETOR
+
+let prod = []; //vazio
+
+let produtos = ['camiseta', 'calça', 'blusa'];
+
+// tamanho da lista
+console.log(produtos.lenght);
+
+// acessando um item na lista
+console.log(produtos[0]);
+
+
+let jogadores = [
+    {
+        nome: 'Beto',
+        posicao: 'Atacante'
+    },
+    {
+        nome: 'Gil',
+        posicao: 'Zagueiro'
+    }
+]
